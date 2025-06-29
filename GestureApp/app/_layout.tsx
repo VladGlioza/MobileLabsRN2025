539922a2f83bce7a868ec41c78889cd2f89f6ff1
Vlad Glioza <vt211_gvv@student.ztu.edu.ua>
2025-06-29 09:50:13 +0300

    lab03 app init

---
+++ GestureApp/app/_layout.tsx
@@ -0,0 +1,42 @@
+import { RootStackParamList } from "@/types/RootStackParamList";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+export { ErrorBoundary } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootLayout() {
+    const [loaded] = useFonts({
+        SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    });
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
+    return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+    return (
+        <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{ headerShown: false }}
+        >
+            <Stack.Screen name="Home" />
+            <Stack.Screen name="Tasks" />
+        </Stack.Navigator>
+    );
+}
